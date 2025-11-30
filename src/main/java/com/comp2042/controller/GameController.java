e51b2dec76eb4325d5d127a9b042a1aa594a4ecb
Moiz Israr <basilmoiz@gmail.com>
2025-11-30 11:16:44 +0800

    refactor: extracted handleBrickLanded helper from onDownEvent

--- src/main/java/com/comp2042/controller/GameController.java
+++ src/main/java/com/comp2042/controller/GameController.java
@@ -27,27 +27,34 @@ public class GameController implements InputEventListener {
         viewGuiController.bindScore(board.getScore().scoreProperty());
     }
 
+    private ClearRow handleBrickLanded() {
+        board.mergeBrickToBackground();
+        ClearRow clearRow = board.clearRows();
+
+        if (clearRow.getLinesRemoved() > 0) {
+            board.getScore().add(clearRow.getScoreBonus());
+        }
+
+        if (board.createNewBrick()) {
+            viewGuiController.gameOver();
+        }
+
+        viewGuiController.refreshGameBackground(board.getBoardMatrix());
+
+        return clearRow;
+    }
+
     @Override
     public DownData onDownEvent(MoveEvent event) {
         boolean canMove = board.moveBrickDown();
         ClearRow clearRow = null;
+
         if (!canMove) {
-            board.mergeBrickToBackground();
-            clearRow = board.clearRows();
-            if (clearRow.getLinesRemoved() > 0) {
-                board.getScore().add(clearRow.getScoreBonus());
-            }
-            if (board.createNewBrick()) {
-                viewGuiController.gameOver();
-            }
-
-            viewGuiController.refreshGameBackground(board.getBoardMatrix());
-
-        } else {
-            if (event.getEventSource() == EventSource.USER) {
-                board.getScore().add(1);
-            }
+            clearRow = handleBrickLanded();
+        } else if (event.getEventSource() == EventSource.USER) {
+            board.getScore().add(1);
         }
+
         return new DownData(clearRow, board.getViewData());
     }
 
