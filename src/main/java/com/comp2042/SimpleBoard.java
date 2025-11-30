866da666997748b63c77a41a165d76a90d581b4a
Moiz Israr <basilmoiz@gmail.com>
2025-11-30 10:49:13 +0800

    refactor: clarified 'game over' check in createNewBrick by introducing a named boolean

--- src/main/java/com/comp2042/SimpleBoard.java
+++ src/main/java/com/comp2042/SimpleBoard.java
@@ -84,7 +84,16 @@ public class SimpleBoard implements Board {
         Brick currentBrick = brickGenerator.getBrick();
         brickRotator.setBrick(currentBrick);
         currentOffset = new Point(SPAWN_X, SPAWN_Y);
-        return MatrixOperations.intersect(currentGameMatrix, brickRotator.getCurrentShape(), (int) currentOffset.getX(), (int) currentOffset.getY());
+
+        boolean intersectsExistingBlocks = MatrixOperations.intersect(
+            currentGameMatrix,
+            brickRotator.getCurrentShape(),
+            (int) currentOffset.getX(),
+            (int) currentOffset.getY()
+        );
+
+        // true here means we collided immediately hence game over
+        return intersectsExistingBlocks;
     }
 
     @Override
