af8bbb5209174fcd86719b115b89309a301364fd
Moiz Israr <basilmoiz@gmail.com>
2025-12-08 06:38:27 +0800

    added Javadoc to BrickGenerator

--- src/main/java/com/comp2042/logic/bricks/BrickGenerator.java
+++ src/main/java/com/comp2042/logic/bricks/BrickGenerator.java
@@ -1,8 +1,29 @@
 package com.comp2042.logic.bricks;
 
+/**
+ * Strategy interface for generating Tetris bricks.
+ * <p>
+ * Implementations decide how new bricks are produced (for example,
+ * completely random or using a preview/queue system).
+ */
 public interface BrickGenerator {
 
+    /**
+     * Returns the current brick that should be active in the game.
+     * <p>
+     * This is typically the brick that the player is currently controlling
+     * on the board.
+     *
+     * @return the current {@link Brick}
+     */
     Brick getBrick();
 
+    /**
+     * Returns the next brick that will follow the current one.
+     * <p>
+     * This value is used by the GUI to show the "next piece" preview.
+     *
+     * @return the next {@link Brick} that will be spawned
+     */
     Brick getNextBrick();
 }
