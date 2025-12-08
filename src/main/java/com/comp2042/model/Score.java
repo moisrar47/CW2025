1af58385a1b1d254d4c929e6206469934ff77a40
Moiz Israr <basilmoiz@gmail.com>
2025-12-08 10:19:51 +0800

    added Javadoc to Score

--- src/main/java/com/comp2042/model/Score.java
+++ src/main/java/com/comp2042/model/Score.java
@@ -3,18 +3,43 @@ package com.comp2042.model;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
+/**
+ * Tracks the player's score using a JavaFX {@link IntegerProperty} so that
+ * the GUI can bind directly to score updates.
+ */
 public final class Score {
 
+    /**
+     * JavaFX property representing the current score value.
+     * Enables automatic GUI updates via property binding.
+     */
     private final IntegerProperty score = new SimpleIntegerProperty(0);
 
+    /**
+     * Returns the JavaFX property storing the score.
+     * <p>
+     * Used by the GUI for real-time HUD binding.
+     *
+     * @return the score property
+     */
     public IntegerProperty scoreProperty() {
         return score;
     }
 
+    /**
+     * Returns the JavaFX property storing the score.
+     * <p>
+     * Used by the GUI for real-time HUD binding.
+     *
+     * @return the score property
+     */
     public void add(int i){
         score.setValue(score.getValue() + i);
     }
 
+    /**
+     * Resets the score back to zero.
+     */
     public void reset() {
         score.setValue(0);
     }
